f1493a7efded3f99d464a84ad9cf74a977f74513
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2025-07-08 18:20:57 -0400

    initial mian class + pom file

---
+++ src/main/java/dev/hr/rezaei/build/flow/BuildFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.hr.rezaei.build.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hr/rezaei/build/flow/BuildFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hr.rezaei.build.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
