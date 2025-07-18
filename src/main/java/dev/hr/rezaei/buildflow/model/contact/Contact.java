8ad819da2a5aec47b9d85056316cd9753fa1a4c2
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2025-07-18 18:23:33 -0400

    contact JPA repo + tests

--- src/main/java/dev/hr/rezaei/buildflow/model/contact/Contact.java
+++ src/main/java/dev/hr/rezaei/buildflow/model/contact/Contact.java
@@ -1,6 +1,7 @@
 package dev.hr.rezaei.buildflow.model.contact;
 
 import dev.hr.rezaei.buildflow.model.address.Address;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +14,35 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "contacts")
 public class Contact {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false)
     private UUID id;
-    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String firstName;
+
+    @Column(length = 100, nullable = false)
+    private String lastName;
+
+    @ElementCollection(targetClass = ContactLabel.class)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "label", length = 50)
     private List<ContactLabel> labels;
+
+    @Column(length = 100)
     private String email;
+
+    @Column(length = 30)
     private String phone;
+
+    @Column(length = 500)
     private String notes;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
     private Address address;
 }
