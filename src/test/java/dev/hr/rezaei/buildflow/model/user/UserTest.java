448beae7cb4f4f6955ef8528db392f446685597f
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2025-07-28 10:54:55 -0400

    User equals & hashCode unit tests

--- src/test/java/dev/hr/rezaei/buildflow/model/user/UserTest.java
+++ src/test/java/dev/hr/rezaei/buildflow/model/user/UserTest.java
@@ -1,28 +1,62 @@
 package dev.hr.rezaei.buildflow.model.user;
 
-import org.junit.jupiter.api.BeforeEach;
+import dev.hr.rezaei.buildflow.model.AbstractModelTest;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.util.UUID;
 
-class UserTest {
-    private Contact contact;
-    private User user;
+import static org.junit.jupiter.api.Assertions.*;
 
-    @BeforeEach
-    void setUp() {
-        contact = Mockito.mock(Contact.class);
-        user = User.builder()
-                .username("testuser")
-                .email("test@example.com")
-                .registered(true)
-                .contact(contact)
-                .build();
-    }
+class UserTest extends AbstractModelTest {
 
     @Test
     void toString_shouldNotThrow_whenNoCycle() {
-        assertDoesNotThrow(user::toString);
+        assertDoesNotThrow(testUser::toString);
+    }
+
+    @Test
+    void equals_shouldReturnTrue_forSameId() {
+        User user1 = User.builder()
+                .id(testUser.getId())
+                .username("user1")
+                .email("user1@example.com")
+                .registered(true)
+                .contact(testUser.getContact())
+                .build();
+        User user2 = User.builder()
+                .id(testUser.getId())
+                .username("user2")
+                .email("user2@example.com")
+                .registered(false)
+                .contact(testUser.getContact())
+                .build();
+        // Should be equal because id is the same
+        assertDoesNotThrow(() -> user1.equals(user2));
+        assertDoesNotThrow(() -> user2.equals(user1));
+        assertDoesNotThrow(user1::hashCode);
+        assertDoesNotThrow(user2::hashCode);
+        assertEquals(user1, user2);
+        assertEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    void equals_shouldReturnFalse_forDifferentId() {
+        User user1 = User.builder()
+                .id(UUID.randomUUID())
+                .username("user1")
+                .email("user1@example.com")
+                .registered(true)
+                .contact(testUser.getContact())
+                .build();
+        User user2 = User.builder()
+                .id(UUID.randomUUID())
+                .username("user1")
+                .email("user1@example.com")
+                .registered(true)
+                .contact(testUser.getContact())
+                .build();
+        // Should not be equal because id is different
+        assertNotEquals(user1, user2);
+        assertNotEquals(user1.hashCode(), user2.hashCode());
     }
 }
